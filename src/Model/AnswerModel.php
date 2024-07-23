66749fc0d4ac3f6f602eb3a36598a73a7c33b1a2
Brice POTÉ <bpote-ext@splio.com>
2024-07-24 01:20:23 +0200

    Create new SF Command for feed tables for Heroku platform without using fixtures and migrations

---
+++ src/Model/AnswerModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Model;
+
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AnswerModel
+{
+	private array $values;
+	private string $username;
+	private string $content;
+	private int $vote;
+
+	public function __construct(array $values)
+	{
+		$this->values = $values;
+		$this->username = $values[0] ?? 'no username';
+		$this->content = $values[1] ?? 'no content';
+		$this->vote = random_int(-20, 20);
+	}
+
+	public function getValues(): array
+	{
+		return $this->values;
+	}
+
+	public function getUsername(): string
+	{
+		return trim($this->username);
+	}
+
+	public function getContent(): string
+	{
+		return trim($this->content);
+	}
+
+	public function getVote(): int
+	{
+		return $this->vote;
+	}
+}
---
+++ src/Model/QuestionModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Model;
+
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class QuestionModel
+{
+	private array $values;
+	private string $name;
+	private string $question;
+	private int $vote;
+	private string $slug;
+
+	public function __construct(array $values, SluggerInterface $slugger)
+	{
+		$this->values = $values;
+		$this->name = $values[0] ?? 'no name';
+		$this->question = $values[1] ?? 'no question';
+		$this->vote = random_int(-20, 20);
+		$this->slug = strtolower($slugger->slug($this->name)->toString()) ?? 'no slug';
+	}
+
+	public function getValues(): array
+	{
+		return $this->values;
+	}
+
+	public function getName(): string
+	{
+		return trim($this->name);
+	}
+
+	public function getQuestion(): string
+	{
+		return trim($this->question);
+	}
+
+	public function getVote(): int
+	{
+		return $this->vote;
+	}
+
+	public function getSlug(): string
+	{
+		return trim($this->slug);
+	}
+
+}
